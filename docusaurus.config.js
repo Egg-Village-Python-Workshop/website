9da9458152bb3cc14c481a2eb311957ab3ef32f6
Duke <hibary.taiwan@gmail.com>
2026-02-23 13:17:28 +0800

    fix(config): Update GA tracking ID

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -59,7 +59,7 @@ const config = {
           customCss: require.resolve("./src/css/custom.css"),
         },
         gtag: {
-          trackingID: "G-1EXVJQQQ1G",
+          trackingID: "GT-NBBRQBF",
           anonymizeIP: true,
         },
       }),
